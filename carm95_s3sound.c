7bf59e63b4d7b73e641cc2ed37ec27f5af3759b6
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-11-03 01:51:58 +0100

    Add S3SoundStillPlaying

--- carm95_s3sound.c
+++ carm95_s3sound.c
@@ -7,3 +7,9 @@ CARM95_HOOK_FUNCTION(original_S3SetEffects, S3SetEffects)
 int __cdecl S3SetEffects(tS3_sample_filter* filter1, tS3_sample_filter* filter2) {
     return original_S3SetEffects(filter1, filter2);
 }
+
+static int(__cdecl*original_S3SoundStillPlaying)(tS3_sound_tag pSound) = (int(__cdecl*)(tS3_sound_tag pSound))0x0049c829;
+CARM95_HOOK_FUNCTION(original_S3SoundStillPlaying, S3SoundStillPlaying)
+int __cdecl S3SoundStillPlaying(tS3_sound_tag pSound) {
+    return original_S3SoundStillPlaying(pSound);
+}
